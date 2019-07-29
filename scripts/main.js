ff14bd5c60f904976a7a1edb9542fc2d0cfd35f1
Manuel Segovia <manuelsegovia@Manuels-MBP.attlocal.net>
2019-07-28 19:28:51 -0500

    finished remote store

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,18 @@
   'use strict';
   var FORM_SELECTOR = '[data-coffee-order="form"]';
   var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var RemoteDataStore = App.RemoteDataStore;
   var FormHandler = App.FormHandler;
   var Validation = App.Validation;
   var CheckList = App.CheckList;
+  var remoteDS = new RemoteDataStore(SERVER_URL);
  
-  var myTruck = new Truck('Odissey', new DataStore());
+  var myTruck = new Truck('Odissey', remoteDS);
   window.myTruck = myTruck;
   var checkList = new CheckList(CHECKLIST_SELECTOR);
   checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
@@ -21,3 +25,8 @@
   });
   formHandler.addInputHandler(Validation.isCompanyEmail);
 })(window);
+
+
+
+
+
---
+++ scripts/remotedatastore.js
@@ -0,0 +1,46 @@
+
+(function(window){
+  'use strict';
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function RemoteDataStore(url){
+    if (!url) {
+      throw new Error('No remote URL supplied.');
+    }
+    this.serverUrl = url;
+  }
+
+  RemoteDataStore.prototype.add = function(key,val){
+    // code here
+    $.post(this.serverUrl,val,function(serverResponse){
+      console.log(serverResponse);
+    });
+  };
+  RemoteDataStore.prototype.getAll= function(cb){
+    $.get(this.serverUrl, function(serverResponse) {
+      console.log(serverResponse);
+      cb(serverResponse);
+    });
+  };
+  RemoteDataStore.prototype.get = function(key, cb){
+    $.get(this.serverUrl + '/' + key, function(serverResponse){
+      console.log(serverResponse);
+      cb(serverResponse);
+    });
+  };
+  RemoteDataStore.prototype.remove = function (key){
+    $.ajax(this.serverUrl + '/' + key, {
+      type: 'DELETE'
+    });
+  };
+  App.RemoteDataStore = RemoteDataStore;
+  window.App = App;
+})(window);
+
+
+
+/* 
+var remoteDS = new App.RemoteDataStore("http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders");
+
+ */
